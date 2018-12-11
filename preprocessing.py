0393fe1f224ab3075bfaa4a7c9da20d914e3dc44
lupita24 <aniluaf1415@gmail.com>
2018-12-10 19:10:16 -0500

    Add files via upload
    preprocesamiento de tweets

---
+++ preprocessing.py
@@ -0,0 +1,170 @@
+import xml.etree.ElementTree as ET
+import re
+# -*- encoding: utf-8 -*-
+
+
+def parse_with_lxml():
+    tree = ET.parse('test.xml')
+    root = tree.getroot()
+    f = open ("limpiotest.txt","w")
+    for content in root.findall('tweet'):
+        cont = content.find('content').text
+        C2 = cont.encode('utf8')
+       # for pola in content.findall('sentiment'):
+        #    for value in pola.findall("./polarity/value"):
+         #       f.write(value.text + " ")
+        f.write(C2)
+    f.close()
+
+def detectar_polaridad(polaridad):
+    val = polaridad.find("NONE")
+    if val != -1:
+        return "neu"
+    val = polaridad.find("P")
+    if val != -1:
+        return "pos"
+    return "neg"
+
+def clasificar_tweet():
+    f = open("limpio.txt","r")
+    neg = open("tweets.neg","w")
+    pos = open("tweets.pos","w")
+    neu = open("tweets.neu","w")
+    cont = 0
+    dato = 0
+    contenido = ''
+    polaridad = ''
+    tweet = ''
+    tt = ""
+
+
+    while cont < 1009:
+        contenido = f.readline()
+        if  len(contenido)>1:
+            if dato == 0:
+                polaridad = contenido
+                dato = 1
+            else:
+                tweet = contenido
+                dato = 2
+
+        if dato == 2:
+            polaridad = detectar_polaridad(polaridad)
+            if polaridad == 'neg':
+                neg.write(tweet)
+            elif polaridad == 'pos':
+                pos.write(tweet)
+            else:
+                neu.write(tweet)
+            dato = 0
+            cont = cont+1
+
+    f.close()
+    neu.close()
+    neg.close()
+    pos.close()
+
+def parser_tweets_pos():
+    cont = 0
+
+    pos = open("tweets.pos", "r")
+    pos_tweet = open("tweets_pos.pos","w")
+    i = 0.0
+
+    while  i < 322:
+        linea = pos.readline()
+        print(linea)
+        new_tweet = ''
+        linea = linea.strip()
+        for word in linea.split():
+            # String preprocessing
+            if re.match('^.*@.*', word):
+                word = '<NAME/>'
+            if re.match('^.*http//.*', word):
+                word = '<LINK/>'
+            if re.match('^.*https//.*', word):
+                    word = '<LINK/>'
+            word = word.replace('#', '<HASHTAG/> ')
+            word = word.replace('&quot;', ' \" ')
+            word = word.replace('&amp;', ' & ')
+            word = word.replace('&gt;', ' > ')
+            word = word.replace('&lt;', ' < ')
+            new_tweet = ' '.join([new_tweet, word])
+        linea = new_tweet.strip() + '\n'
+        pos_tweet.write(linea)
+        i =i+1.0
+    print(i)
+    pos.close()
+    pos_tweet.close()
+
+def parser_tweets_neg():
+    cont = 0
+
+    neg = open("tweets.neg", "r")
+    neg_tweet = open("tweets_neg.neg","w")
+    i = 0.0
+
+    while  i < 551:
+        linea = neg.readline()
+        print(linea)
+        new_tweet = ''
+        linea = linea.strip()
+        for word in linea.split():
+            # String preprocessing
+            if re.match('^.*@.*', word):
+                word = '<NAME/>'
+            if re.match('^.*http//.*', word):
+                word = '<LINK/>'
+            if re.match('^.*https//.*', word):
+                word = '<LINK/>'
+            word = word.replace('#', '<HASHTAG/> ')
+            word = word.replace('&quot;', ' \" ')
+            word = word.replace('&amp;', ' & ')
+            word = word.replace('&gt;', ' > ')
+            word = word.replace('&lt;', ' < ')
+            new_tweet = ' '.join([new_tweet, word])
+        linea = new_tweet.strip() + '\n'
+        neg_tweet.write(linea)
+        i =i+1.0
+    print(i)
+    neg.close()
+    neg_tweet.close()
+
+def parser_tweets_test():
+    cont = 0
+
+    pos = open("limpiotest.txt", "r")
+    pos_tweet = open("test3.txt","w")
+    i = 0.0
+
+    while  i < 1690:
+        linea = pos.readline()
+        print(linea)
+        new_tweet = ''
+        linea = linea.strip()
+        for word in linea.split():
+            # String preprocessing
+            if re.match('^.*@.*', word):
+                word = '<NAME/>'
+            if re.match('^.*http//.*', word):
+                word = '<LINK/>'
+            if re.match('^.*https//.*', word):
+                    word = '<LINK/>'
+            word = word.replace('#', '<HASHTAG/> ')
+            word = word.replace('&quot;', ' \" ')
+            word = word.replace('&amp;', ' & ')
+            word = word.replace('&gt;', ' > ')
+            word = word.replace('&lt;', ' < ')
+            new_tweet = ' '.join([new_tweet, word])
+        linea = new_tweet.strip() + '\n'
+        pos_tweet.write(linea)
+        i =i+1.0
+    print(i)
+    pos.close()
+    pos_tweet.close()
+
+if __name__ == '__main__':
+# parse_with_lxml()  
+#clasificar_tweet()
+    parser_tweets_test()
+ #   parser_tweets_pos()
